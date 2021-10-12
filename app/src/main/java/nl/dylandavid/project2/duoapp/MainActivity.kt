63e6a6edccf392a26ae193fd03f8e9be4d3af923
kurza <kurzacationer@gmail.com>
2021-10-12 15:52:57 +0200

    Init

---
+++ app/src/main/java/nl/dylandavid/project2/duoapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package nl.dylandavid.project2.duoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
